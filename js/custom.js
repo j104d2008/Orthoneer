14c4289104c75dad3667e58c9c3435024694186b
j104d2008 <66717407+j104d2008@users.noreply.github.com>
2026-01-29 01:21:51 +0700

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -119,14 +119,14 @@
   });
 
   // menu fixed js code
-  $(window).scroll(function () {
-    var window_top = $(window).scrollTop() + 1;
-    if (window_top > 50) {
-      $('.main_menu').addClass('menu_fixed animated fadeInDown');
-    } else {
-      $('.main_menu').removeClass('menu_fixed animated fadeInDown');
-    }
-  });
+//  $(window).scroll(function () {
+//    var window_top = $(window).scrollTop() + 1;
+//    if (window_top > 50) {
+//      $('.main_menu').addClass('menu_fixed animated fadeInDown');
+//    } else {
+//      $('.main_menu').removeClass('menu_fixed animated fadeInDown');
+//    }
+//  });
 
   $('.counter').counterUp({
     time: 2000
